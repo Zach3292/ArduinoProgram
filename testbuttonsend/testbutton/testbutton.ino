130323dc52a610904e32482a5943c697f6c600eb
Zach <zroy@leseminaire.ca>
2019-02-06 15:43:41 -0500

    modify testbuttonsend

--- testbuttonsend/testbutton/testbutton.ino
+++ testbuttonsend/testbutton/testbutton.ino
@@ -14,7 +14,7 @@ int buttonPin = 12;  //buttonpin pourrait s’appeler bouton_yeux. À vous de ch
 int LED = 13;    //Même chose ici. Au lieu de LED, on pourrait dire oeil_gauche.
 IRsend irsend;
 void setup() {
- 
+ Serial.begin(9600);
    // Definir le port #12 comme une entrée (capteur) et activer la résistance interne.
    pinMode(buttonPin, INPUT_PULLUP);  // Si vous avez changé le nom de buttonpin plus haut, utiliser le même nom ici
    // Définir le port 13 comme une sortie. L’électricité ira vers la DEL.
@@ -30,6 +30,7 @@ void loop(){
       digitalWrite(LED,HIGH);
        irsend.sendSony(0x18E758A7, 32);
                 Serial.println("Sending");
+                delay(15);
    } else {
       // Sinon, on étain la DEL. (LOW)
       digitalWrite(LED, LOW);
