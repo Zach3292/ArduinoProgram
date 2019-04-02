b4c29aaa05f350842ad5b4a8f57f194f04ceaf81
Zach3292 <39390866+Zach3292@users.noreply.github.com>
2019-04-02 15:16:29 -0400

    Add comment

--- lasertag/lasertag.ino
+++ lasertag/lasertag.ino
@@ -3,17 +3,17 @@
 #include <IRremoteInt.h>
 #include <ir_Lego_PF_BitStreamEncoder.h>
 
-int redLED = 12;
+int redLED = 12;  //initialize all pin on vest and the Ir sensor
 int blueLED = 9;
 int RECV_PIN = 5;
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 
-#define BUTTON_1 0x18E758A7
+#define BUTTON_1 0x18E758A7 // define the button ir code in hexadecimal
 #define BUTTON_2 0xFF18E7
 void setup()
 {
-  pinMode(redLED, OUTPUT);
+  pinMode(redLED, OUTPUT);  //set the pinmode of the led and start the serial monitor and sensor
   pinMode(blueLED, OUTPUT);
   irrecv.enableIRIn();
   Serial.begin(9600);
@@ -21,13 +21,13 @@ void setup()
 }
 void loop() {
 
-  if(irrecv.decode(&results)){
+  if(irrecv.decode(&results)){  //if the sensor see something
   
-    Serial.print(results.value, HEX);
-    if (results.value == BUTTON_1)
+    Serial.print(results.value, HEX); //print the value in hexadecimal value
+    if (results.value == BUTTON_1)  // if someone shoot you then:
     {
       Serial.print(results.value, HEX);
-     digitalWrite(blueLED, LOW);
+     digitalWrite(blueLED, LOW); // make the two led flash
     
       digitalWrite(redLED, HIGH);
       delay(500);
@@ -41,9 +41,9 @@ void loop() {
     }
     
     else{
-      Serial.print("Rien");
+      Serial.print("Rien"); //else print RIEN
       }
     
-    irrecv.resume();
+    irrecv.resume();  // Restart the sensor
   }
   }
