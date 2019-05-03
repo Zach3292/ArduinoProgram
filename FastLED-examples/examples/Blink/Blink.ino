681d78b1f5bde19f318c3a125095722c344895ab
Zacharie Roy <zroy@leseminaire.ca>
2019-05-02 20:54:17 -0400

    Add led strip velo

--- FastLED-examples/examples/Blink/Blink.ino
+++ FastLED-examples/examples/Blink/Blink.ino
@@ -1,7 +1,7 @@
 #include <FastLED.h>
 
 // How many leds in your strip?
-#define NUM_LEDS 1
+#define NUM_LEDS 150
 
 // For led chips like Neopixels, which have a data line, ground, and power, you just
 // need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
@@ -19,7 +19,7 @@ void setup() {
       // FastLED.addLeds<TM1809, DATA_PIN, RGB>(leds, NUM_LEDS);
       // FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
       // FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
-        FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
+        FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
   	//FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
       // FastLED.addLeds<APA104, DATA_PIN, RGB>(leds, NUM_LEDS);
       // FastLED.addLeds<UCS1903, DATA_PIN, RGB>(leds, NUM_LEDS);
@@ -43,12 +43,11 @@ void setup() {
 }
 
 void loop() { 
-  // Turn the LED on, then pause
-  leds[0] = CRGB::Red;
-  FastLED.show();
-  delay(500);
-  // Now turn the LED off, then pause
-  leds[0] = CRGB::Black;
-  FastLED.show();
-  delay(500);
+  for(int dot = 0; dot < NUM_LEDS; dot++) { 
+            leds[dot] = CRGB::Yellow;
+            FastLED.show();
+            // clear this led for the next time around the loop
+            leds[dot] = CRGB::Black;
+            delay(100);
+        }
 }
--- LED_STRIP_velo/LED_STRIP_velo.ino
+++ LED_STRIP_velo/LED_STRIP_velo.ino
@@ -8,7 +8,7 @@
 CRGB leds[NUM_LEDS];
 
 #define UPDATES_PER_SECOND 100
-
+int buttonPin = 12;
 // This example shows several ways to set up and use 'palettes' of colors
 // with FastLED.
 //
@@ -36,6 +36,7 @@ extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
 
 
 void setup() {
+    pinMode(buttonPin, INPUT_PULLUP);
     delay( 3000 ); // power-up safety delay
     FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
     FastLED.setBrightness(  BRIGHTNESS );
@@ -47,6 +48,7 @@ void setup() {
 
 void loop()
 {
+   
     ChangePalettePeriodically();
     
     static uint8_t startIndex = 0;
@@ -79,22 +81,14 @@ void FillLEDsFromPaletteColors( uint8_t colorIndex)
 
 void ChangePalettePeriodically()
 {
-    uint8_t secondHand = (millis() / 1000) % 60;
-    static uint8_t lastSecond = 99;
+    int buttonValue = digitalRead(buttonPin);
+    uint8_t secondHand = (millis() / 1000) % 5;
+    static uint8_t lastSecond = 20;
     
     if( lastSecond != secondHand) {
         lastSecond = secondHand;
-        if( secondHand ==  0)  { currentPalette = RainbowColors_p;         currentBlending = LINEARBLEND; }
-        if( secondHand == 10)  { currentPalette = RainbowStripeColors_p;   currentBlending = NOBLEND;  }
-        if( secondHand == 15)  { currentPalette = RainbowStripeColors_p;   currentBlending = LINEARBLEND; }
-        if( secondHand == 20)  { SetupPurpleAndGreenPalette();             currentBlending = LINEARBLEND; }
-        if( secondHand == 25)  { SetupTotallyRandomPalette();              currentBlending = LINEARBLEND; }
-        if( secondHand == 30)  { SetupBlackAndWhiteStripedPalette();       currentBlending = NOBLEND; }
-        if( secondHand == 35)  { SetupBlackAndWhiteStripedPalette();       currentBlending = LINEARBLEND; }
-        if( secondHand == 40)  { currentPalette = CloudColors_p;           currentBlending = LINEARBLEND; }
-        if( secondHand == 45)  { currentPalette = PartyColors_p;           currentBlending = LINEARBLEND; }
-        if( secondHand == 50)  { currentPalette = myRedWhiteBluePalette_p; currentBlending = NOBLEND;  }
-        if( secondHand == 55)  { currentPalette = myRedWhiteBluePalette_p; currentBlending = LINEARBLEND; }
+        if( secondHand == 0)  { currentPalette = RainbowColors_p;   currentBlending = LINEARBLEND; }
+        if(buttonValue == LOW) { SetupPurpleAndGreenPalette();             currentBlending = LINEARBLEND; }
     }
 }
 
@@ -125,15 +119,14 @@ void SetupBlackAndWhiteStripedPalette()
 // This function sets up a palette of purple and green stripes.
 void SetupPurpleAndGreenPalette()
 {
-    CRGB purple = CHSV( HUE_PURPLE, 255, 255);
-    CRGB green  = CHSV( HUE_GREEN, 255, 255);
+    CRGB red  = CHSV( HUE_RED, 255, 255);
     CRGB black  = CRGB::Black;
     
     currentPalette = CRGBPalette16(
-                                   green,  green,  black,  black,
-                                   purple, purple, black,  black,
-                                   green,  green,  black,  black,
-                                   purple, purple, black,  black );
+                                   red,  red, black,  black,
+                                   red,  red, black,  black,
+                                   red,  red, black,  black,
+                                   red,  red, black,  black );
 }
 
 
