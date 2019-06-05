9c195c9a30b62030b8224363ac0359948908c9d7
Zach3292 <39390866+Zach3292@users.noreply.github.com>
2019-06-05 15:01:00 -0400

    Create themperature.ino

---
+++ themperature.ino
@@ -0,0 +1,26 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 8
+
+OneWire oneWire(ONE_WIRE_BUS);
+
+DallasTemperature sensors(&oneWire);
+
+float Celsius = 0;
+
+void setup() {
+  sensors.begin();
+  Serial.begin(9600);
+}
+
+void loop() {
+  sensors.requestTemperatures();
+
+  Celsius = sensors.getTempCByIndex(0);
+ Serial.print(" C  ");
+  Serial.println(Celsius);
+  
+  
+  delay(1000);
+}
