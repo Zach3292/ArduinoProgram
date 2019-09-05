b406bbef0895bc6c48038d5d3c5adb62f70a2ae5
Zach3292 <zroy@leseminaire.ca>
2019-09-05 15:17:13 -0400

    make led matrix work

--- ledmatrix/ledmatrix.ino
+++ ledmatrix/ledmatrix.ino
@@ -1,8 +1,11 @@
 #include "LedControl.h"
 
 
+const int trigPin = 5;
+const int echoPin = 6;
 
-
+long duration;
+int distance;
 
 LedControl lc=LedControl(12,10,11,1);  // Pins: DIN,CLK,CS, # of Display connected
 
@@ -18,47 +21,7 @@ byte basic_eye[] = {
   B01111110,
   B00111100};
 
-byte bottom_right_eye[] = { 
-  B00111100,
-  B01111110,
-  B11111111,
-  B11111111,
-  B11111111,
-  B11110011,
-  B01110010,
-  B00111100};
 
-
-byte bottom_left_eye[] = { 
-  B00111100,
-  B01111110,
-  B11111111,
-  B11111111,
-  B11111111,
-  B11001111,
-  B01001110,
-  B00111100};
-
-
-byte top_right_eye[] = { 
-  B00111100,
-  B01110010,
-  B11110011,
-  B11111111,
-  B11111111,
-  B11111111,
-  B01111110,
-  B00111100};
-
-byte top_left_eye[] = { 
-  B00111100,
-  B01001110,
-  B11001111,
-  B11111111,
-  B11111111,
-  B11111111,
-  B01111110,
-  B00111100};
 
 byte right_eye[] = { 
   B00111100,
@@ -80,37 +43,41 @@ byte left_eye[] = {
   B01111110,
   B00111100};
 
-byte top_eye[] = { 
+byte middle_left_eye[] = { 
   B00111100,
-  B01100110,
-  B11100111,
+  B01111110,
   B11111111,
-  B11111111,
+  B11001111,
+  B11001111,
   B11111111,
   B01111110,
   B00111100};
 
-byte bottom_eye[] = { 
+  byte middle_right_eye[] = { 
   B00111100,
   B01111110,
   B11111111,
+  B11110011,
+  B11110011,
   B11111111,
-  B11111111,
-  B11100111,
-  B01100110,
+  B01111110,
   B00111100};
+
+
 
 void setup()
 {
   lc.shutdown(0,false);  // Wake up displays
   lc.setIntensity(0,5);  // Set intensity levels
   lc.clearDisplay(0);  // Clear Displays
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   Serial.begin(9600);
 }
 
 
 //  Take values in Arrays and Display them
-void basiceye_void()
+void basic_eye_void()
 {
   for (int i = 0; i < 8; i++)  
   {
@@ -118,34 +85,7 @@ void basiceye_void()
   }
 }
 
-void bottom_right_eye_void()
-{
-  for (int i = 0; i < 8; i++)
-  {
-    lc.setRow(0,i,bottom_right_eye[i]);
-  }
-}
-void bottom_left_eye_void()
-{
-  for (int i = 0; i < 8; i++)
-  {
-    lc.setRow(0,i,bottom_left_eye[i]);
-  }
-}
-void top_right_eye_void()
-{
-  for (int i = 0; i < 8; i++)
-  {
-    lc.setRow(0,i,top_right_eye[i]);
-  }
-}
-void top_left_eye_void()
-{
-  for (int i = 0; i < 8; i++)
-  {
-    lc.setRow(0,i,top_left_eye[i]);
-  }
-}
+
 void right_eye_void()
 {
   for (int i = 0; i < 8; i++)
@@ -160,18 +100,20 @@ void left_eye_void()
     lc.setRow(0,i,left_eye[i]);
   }
 }
-void top_eye_void()
+
+void middle_left_eye_void()
 {
   for (int i = 0; i < 8; i++)
   {
-    lc.setRow(0,i,top_eye[i]);
+    lc.setRow(0,i,middle_left_eye[i]);
   }
 }
-void bottom_eye_void()
+
+void middle_right_eye_void()
 {
   for (int i = 0; i < 8; i++)
   {
-    lc.setRow(0,i,bottom_eye[i]);
+    lc.setRow(0,i,middle_right_eye[i]);
   }
 }
 
@@ -179,20 +121,39 @@ void bottom_eye_void()
 void loop()
 {
 
-   left_eye_void();
-   delay(100);
-   top_left_eye_void();
-   delay(100);
-   top_eye_void();
-   delay(100);
-   top_right_eye_void();
-   delay(100);
-   right_eye_void();
-   delay(100);
-   bottom_right_eye_void();
-   delay(100);
-   bottom_eye_void();
-   delay(100);
-   bottom_left_eye_void();
-   delay(100);
+    
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    // Sets the trigPin on HIGH state for 10 micro seconds
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    duration = pulseIn(echoPin, HIGH);
+    // Calculating the distance
+    distance= duration*0.034/2;
+    // Prints the distance on the Serial Monitor
+    Serial.print("Distance: ");
+    Serial.println(distance);
+    switch (distance)
+    {
+    case 10:
+      left_eye_void();
+      break;
+    case 20:
+        middle_left_eye_void();
+      break;
+    case 30:
+      basic_eye_void();
+      break;
+    case 40:
+      middle_right_eye_void();
+      break;
+    case 50:
+      right_eye_void();
+      break; 
+    
+    default:
+      break;
+    }
   }
